9c2f8db9ea6973556c0ff35b699d0832658eb19d
jyroball <jyrojimenez@gmail.com>
2025-07-08 02:27:26 -0700

    added classifier into main

--- main.py
+++ main.py
@@ -16,15 +16,34 @@ def main():
 
     #check file extension to use different function
     if path.lower().endswith(".pdf"):
-        text = extract_pdf(path)
+        parsed_text = extract_pdf(path)
     elif path.lower().endswith((".jpg", ".jpeg", ".png")):
-        text = extract_image(path)
+        parsed_text = extract_image(path)
     else:
         print("Unsupported file type.")
         return
 
-    print("\nText: \n")
-    print(text[:1000])  # Just print the first 1000 chars for now
+    #Show parsed document in text form for debugg
+    print("\nParsed Text: \n")
+    print(parsed_text[:1000])  # Just print the first 1000 chars for now
+
+    #get what the document type is from langchain
+    doc_type = classify_document(parsed_text)
+    #output doc type for debug
+    print(f"Document Type: {doc_type}")
+
+    #save document as a json with type of document and parsed value
+    document_data = {
+        #just have document type and content for now, maybe add an ID later?
+        #so we can differentiate between different documents from diff users?
+        #also maybe so we can add to a DB like postgres later?
+        "type": doc_type,
+        "text": parsed_text
+    }
+
+    #create a json file to store data in
+    with open("documents.json", "w", encoding = "utf-8") as f:
+            json.dump(document_data, f, indent = 2)
 
 if __name__ == "__main__":
     main()
